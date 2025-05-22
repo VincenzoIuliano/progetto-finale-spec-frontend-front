aa3028fc61fc69581af0100c86f1c43af970dc9a
Vincenzo Iuliano <iuliano-vincenzo@live.it>
2025-05-22 17:36:49 +0200

    Add GameCard component and update game images in JSON data

---
+++ components/GameCard.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const GameCard = ({ game }) => {
+  return (
+    <div style={{
+      border: '1px solid #ccc',
+      borderRadius: '8px',
+      padding: '16px',
+      width: '300px',
+      boxShadow: '0 4px 8px rgba(0,0,0,0.1)',
+    }}>
+      <img
+        src={game.image}
+        alt={game.title}
+        style={{ width: '100%', borderRadius: '8px' }}
+      />
+      <h2>{game.title}</h2>
+      <p><strong>Categoria:</strong> {game.category}</p>
+      <p>{game.description}</p>
+      <p><strong>Piattaforme:</strong> {game.platforms.join(', ')}</p>
+      <p><strong>Online:</strong> {game.multiplayer_online ? "Sì" : "No"}</p>
+      <p><strong>Locale:</strong> {game.multiplayer_local ? "Sì" : "No"}</p>
+      <p><strong>Coop:</strong> {game.coop ? "Sì" : "No"}</p>
+    </div>
+  );
+};
+
+export default GameCard;
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import React from 'react'
+import GameCard from '../components/gameCard'
+import games from '../database/game.json'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      Test
-    </>
+     <div style={{ padding: '20px' }}>
+      <h1>Videogiochi</h1>
+      <div style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        gap: '20px',
+      }}>
+        {games.map((game, index) => (
+          <GameCard key={index} game={game} />
+        ))}
+      </div>
+    </div>
   )
 }
 
