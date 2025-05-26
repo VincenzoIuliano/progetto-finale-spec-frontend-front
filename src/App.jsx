33ee81a1687ed71287a8d90b7aad3d0db0dd0ad7
Vincenzo Iuliano <iuliano-vincenzo@live.it>
2025-05-26 12:04:24 +0200

    Add routing and homepage: implement navigation and welcome message

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,48 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { NavLink } from 'react-router-dom'
 import GameCard from '../components/gameCard'
 import games from '../database/game.json'
 import './App.css'
+import HomePage from './pages/HomePage'
+
 
 function App() {
 
   return (
-     <div style={{ padding: '20px' }}>
-      <div style={{
-        display: 'flex',
-        flexWrap: 'wrap',
-        gap: '20px',
-      }}>
-        {games.map((game, index) => (
-          <GameCard key={index} game={game} />
-        ))}
+    <BrowserRouter>
+      <nav className="navbar">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/gamelist">Lista dei giochi</NavLink>
+        <NavLink to="/comparator">Comparatore</NavLink>
+      </nav>
+
+      <div className="App">
+        <Routes>
+          {/* Rotta default per mostrare Homepage con messaggio di benvenuto */}
+          <Route path="/" element={<HomePage />} />
+
+          {/* Route per mostrare la lista dei giochi*/}
+          <Route
+            path="/gamelist"
+            element={
+              <div className="game-list">
+                {games.map((game, index) => (
+                  <GameCard key={index} game={game} />
+                ))}
+              </div>
+            }
+          />
+
+          {/* Route per il comparatore*/}
+          <Route path="/comparator" element={<h1>Comparatore</h1>}></Route>
+        </Routes>
       </div>
-    </div>
-  )
+      <footer className="footer">
+        <p>© 2025 Italian Games Comparator. Made by Boolean.</p>
+      </footer>
+    </BrowserRouter>
+  );
 }
 
 export default App
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,21 @@
+export default function HomePage(){
+    return (
+        <div className="home">
+            <h1>Benvenuto su Italian Games Comparator!</h1>
+            <h2>
+                Il primo sito italiano che ti aiuterà a scegliere il prossimo gioco a
+                cui giocherai! La tua nuova avventura inizia qui!
+            </h2>
+            <p>
+                Visita la sezione <a href="/gamelist">Lista completa dei giochi</a> per
+                vedere tutti i giochi disponibili.
+            </p>
+            <p>
+                Per confrontare i giochi, vai alla sezione <a href="/comparator">Comparatore</a>.
+            </p>
+    </div>
+    )
+}
+
+
+
